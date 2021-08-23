4f83d3751dd74346f93385f64ed92beb52681073
rohanbansal12 <rohan2003bansal@gmail.com>
2021-08-23 14:37:26 -0500

    Update

--- updating/util/github_util.py
+++ updating/util/github_util.py
@@ -3,7 +3,8 @@ def update_type_table_repo(repo, file_name, new_table_html, commit_msg="Update")
     index_string = contents.decoded_content.decode().replace("> ", ">")
     first_split = index_string.split('<div class="table100">')
     prepend = first_split[0]
-    postpend = first_split[1].split("</table></div>")[1]
+    postpend_f = first_split[1].split("</table>")[1]
+    postpend = postpend_f.split("</div>")[1]
     new_index_string = prepend + new_table_html + postpend
     response = repo.update_file(file_name, commit_msg, new_index_string, contents.sha)
     return response
